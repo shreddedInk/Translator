294f891b7601e4f31cd0fab03622b328c32d5d6c
Vyacheslav Markov <slavacor8001@gmail.com>
2025-04-16 16:19:48 +0600

    some work for TypeExpression

--- src/main/java/org/example/java/type_control/ParamRecord.java
+++ src/main/java/org/example/java/type_control/ParamRecord.java
@@ -1,2 +1,19 @@
-package org.example.java.type_control;public class ParamRecord {
+package org.example.java.type_control;
+
+public class ParamRecord<T> {
+    private String name;
+    private T value;
+
+    public ParamRecord(String name, T value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public T getValue() {
+        return value;
+    }
 }
--- src/main/java/org/example/java/type_control/TypeExpression.java
+++ src/main/java/org/example/java/type_control/TypeExpression.java
@@ -1,42 +1,116 @@
 package org.example.java.type_control;
 
-public class TypeExpression {
-    private class Node{
-        private Node parent;
-        private TypeI type;
+import java.util.Map;
+import java.util.Objects;
 
-        Node(Node parent, TypeI type){
-            this.parent=parent;
-            this.type=type;
+public class TypeExpression {
+    private Node head;
+    public class Node{
+        private Node parent;
+        private final String name;
+        private Integer start;
+        private Integer end;
+        private Map<String,Node> params;
+
+
+        public Node(String name) {
+            this.name = name;
+            this.parent=null;
+            this.start = this.end = null;
+            this.params = null;
+        }
+
+        public Node(Node parent) {
+            this.parent = parent;
+            this.name = "pointer to "+parent.name;
+            this.start = this.end = null;
+            this.params = null;
+        }
+
+        public Node(Node parent, Integer start, Integer end) {
+            this.parent = parent;
+            this.name = "array of "+parent.name;
+            this.start = start;
+            this.end = end;
+        }
+
+        public Node(Map<String, Node> params) {
+            this.name = "record";
+            this.params = params;
+        }
+
+        public Node getParent() {
+            return parent;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Integer getStart() {
+            return start;
+        }
+
+        public Integer getEnd() {
+            return end;
+        }
+
+        public Map<String, Node> getParams() {
+            return params;
+        }
+
+        @Override
+        public String toString() {
+            if (params!=null){
+                StringBuilder stringBuilder = new StringBuilder("record{");
+                for (var pair: params.entrySet()){
+                    stringBuilder.append(pair.getKey());
+                    stringBuilder.append(":");
+                    stringBuilder.append(pair.getValue());
+                    stringBuilder.append(", " );
+                }
+                return stringBuilder.toString();
+            }
+            else if(start!=null){
+                return "array of "+parent+"["+start+"..."+end+"]";
+            }
+            else if (parent!=null){
+                return "*"+parent;
+            }
+            else return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return Objects.equals(parent, node.parent) && Objects.equals(name, node.name) && Objects.equals(start, node.start) && Objects.equals(end, node.end) && Objects.equals(params, node.params);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(parent, name, start, end, params);
         }
     }
 
-    public static TypeI createPrimitiveType(String name){
-        return new Primitive(name);
-//        Node node = new Node(null,p);
+    private Node integer(){
+        return new Node("integer");
+    }
+    private Node real(){
+        return new Node("real");
     }
 
-    public static TypeI array(int start,int end,TypeI type){
-        return type.array(start,end);
+    private Node pointerTo(Node name){
+        return new Node(name);
     }
 
-    public static TypeI pointerTo(TypeI type){
-        return type.pointer();
+    private Node array(int start, int end, Node type){
+            return new Node(type,start,end);
     }
 
-    public static TypeI record(String name, String paramName1, TypeI paramType1){
-        return new Record(name,paramName1,paramType1);
+    private Node isPointer(Node node){
+        return node.getParent();
     }
-    public static TypeI record(String name, String paramName1, TypeI paramType1,
-                               String paramName2, TypeI paramType2){
-        return new Record(name,paramName1,paramType1,paramName2,paramType2);
 
-    }public static TypeI record(String name, String paramName1, TypeI paramType1,
-                                String paramName2, TypeI paramType2,
-                                String paramName3, TypeI paramType3){
-        return new Record(name,paramName1, paramType1,
-                paramName2, paramType2,
-                paramName3, paramType3);
-
-    }
 }
